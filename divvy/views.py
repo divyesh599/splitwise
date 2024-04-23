4da346509b0e59d0b0938951faae1bf2edc76d0a
divyesh ranpariya <divyeshranpariya599@gmail.com>
2024-04-23 19:03:23 +0530

    added index.html & modified veiw.py

--- divvy/views.py
+++ divvy/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from divvy.models import User, Expense, ExpensePaidBy, ExpenseOwedBy
 
 # Create your views here.
+def homePage(request):
+    # return HttpResponse("Welcome to my Django Project")
+    return render(request, "index.html")
+
+
+def allUser(request):
+    data = User.objects.all().values()
+    return HttpResponse(data)
